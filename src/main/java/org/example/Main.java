aa7d9ee0f96da8ec54d39c0e88bc7858404795c4
Santiago <Santiagoforo@gmail.com>
2025-01-25 11:49:27 -0500

    Commit test to see connection on GitHub Platform.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,9 @@
+
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to JavaBank ATM! Version control with Git.");
+        System.out.println("This is a test for see connection on GitHub Platform");
     }
 }
 
