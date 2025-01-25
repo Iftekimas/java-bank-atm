c9a4c519e32b5d9a12cd1a9dc41f896e84f31426
Santiago <Santiagoforo@gmail.com>
2025-01-25 12:33:56 -0500

    Test ended, succesfull connection.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,8 +3,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to JavaBank ATM! Version control with Git.");
-        System.out.println("This is a test for see connection on GitHub Platform");
-        System.out.println("Another Test for connectivity");
     }
 }
 
